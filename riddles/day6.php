1ec453d0f17bd6543ba1bc37946508f0fee1c32e
Tobias Franz <cinex.mail@web.de>
2021-12-06 10:11:36 +0100

    Improved version for Day 6
    Signed-off-by: Tobias Franz <tobias.franz@mydata.de>

--- riddles/day6.php
+++ riddles/day6.php
@@ -7,8 +7,15 @@ include_once '../InputHelper.php';
 
 const INPUT_FILE = '../inputs/day6';
 
-$inputData   = InputHelper::readFileAsString(INPUT_FILE);
-$initialAges = explode(',', $inputData);
+$inputData        = InputHelper::readFileAsString(INPUT_FILE);
+$initialAges      = explode(',', $inputData);
+$fishGroupedByAge = [];
+foreach ($initialAges as $age) {
+    if (!isset($fishGroupedByAge[$age])) {
+        $fishGroupedByAge[$age] = 0;
+    }
+    $fishGroupedByAge[$age]++;
+}
 
 /**
  * @param array $lanternFish
@@ -16,22 +23,18 @@ $initialAges = explode(',', $inputData);
  */
 function updateInternalTimers(array $lanternFish): array
 {
-    $newFishCount = count(array_filter($lanternFish, function ($fish) {
-        return $fish === 0;
-    }));
-
-    $lanternFish = array_map(function ($fish) {
-        $createdFish = $fish === 0;
-        return $createdFish ? 6 : $fish - 1;
-    }, $lanternFish);
-
-    if ($newFishCount > 0) {
-        for ($i = 1; $i <= $newFishCount; $i++) {
-            $lanternFish[] = 8;
-        }
+    $newGeneration = [];
+    foreach ($lanternFish as $fishAge => $fish) {
+        $newGeneration[$fishAge - 1] = $fish;
     }
 
-    return $lanternFish;
+    if (isset($newGeneration[-1])) {
+        $newGeneration[8] = $newGeneration[-1];
+        $newGeneration[6] = $newGeneration[-1] + ($newGeneration[6] ?? 0);
+        unset($newGeneration[-1]);
+    }
+
+    return $newGeneration;
 }
 
 /**
@@ -42,21 +45,15 @@ function ageFish(array $lanternFish, int $days)
 {
     $start = microtime(true);
     for ($day = 0; $day < $days; $day++) {
-
-        if ($day % floor($days / 20) === 0) {
-            $end = microtime(true);
-            echo sprintf('Day %d of %d (%fs)%s', $day, $days, $end - $start, PHP_EOL);
-            $start = microtime(true);
-        }
         $lanternFish = updateInternalTimers($lanternFish);
     }
     $end = microtime(true);
 
-    echo sprintf('Number of total fish after %d days: %d (%f)s%s', $days, count($lanternFish), $end - $start, PHP_EOL);
+    echo sprintf('Number of total fish after %d days: %d (%f)s%s', $days, array_sum($lanternFish), $end - $start, PHP_EOL);
 }
 
 // Task 1
-ageFish($initialAges, 80);
+ageFish($fishGroupedByAge, 80);
 
 // Task 2
-ageFish($initialAges, 256);
+ageFish($fishGroupedByAge, 256);
